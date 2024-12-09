b8283c73e87f61122d9ecdbff25170baefc4c0e5
agnieszkawojdecka <agnieszka.wojdecka@wp.pl>
2024-12-09 17:47:17 +0100

    Create transaction model

--- apps/transactions/models.py
+++ apps/transactions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Transaction(models.Model):
+    class Types(models.TextChoices):
+        INCOME = "INCOME"
+        EXPENSE = "EXPENSE"
+
+    title = models.CharField(max_length=255)
+    transaction_type = models.CharField(max_length=10, choices=Types)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    # category = models.ForeignKey('categories.Category', on_delete=models.SET_NULL, null=True, related_name="transactions")
+    date = models.DateTimeField(default=timezone.now, blank=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.transaction_type} ({self.amount})"
