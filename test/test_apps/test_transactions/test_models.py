e8b6edc367ed2669abd1c32c5be13ec9d04ea775
agnieszkawojdecka <agnieszka.wojdecka@wp.pl>
2024-12-10 12:34:44 +0100

    Add create category test

--- test/test_apps/test_transactions/test_models.py
+++ test/test_apps/test_transactions/test_models.py
@@ -1,5 +1,5 @@
 import pytest
-from apps.transactions.models import Transaction
+from apps.transactions.models import Transaction, Category
 
 
 @pytest.mark.django_db
@@ -13,3 +13,14 @@ def test_create_transaction():
     assert transaction.title == "Salary"
     assert transaction.transaction_type == "INCOME"
     assert transaction.amount == 5000.00
+
+
+@pytest.mark.django_db
+def test_create_category():
+    category = Category.objects.create(
+        title="Salary",
+        transaction_type="INCOME",
+    )
+
+    assert category.title == "Salary"
+    assert category.transaction_type == "INCOME"
