f91f0906dd6cde6ea018ad52ac998cd960786b43
agnieszkawojdecka <agnieszka.wojdecka@wp.pl>
2024-12-11 11:50:28 +0100

    Add authentication

--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from .filters import TransactionFilter
 from .models import Transaction, Category
@@ -11,11 +12,19 @@ class TransactionListCreateView(generics.ListCreateAPIView):
     serializer_class = TransactionSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = TransactionFilter
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user) # Return transactions for the logged-in user
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user) # Automatically set the user to the logged-in user
 
 
 class TransactionDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class CategoryListCreateView(generics.ListCreateAPIView):
