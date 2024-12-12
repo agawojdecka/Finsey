4c9e1a9841a5565bf92931d9cfee95de8693e395
agnieszkawojdecka <agnieszka.wojdecka@wp.pl>
2024-12-12 11:52:53 +0100

    Add description attribute to Purpose model

--- apps/savings/models.py
+++ apps/savings/models.py
@@ -25,6 +25,7 @@ class Saving(models.Model):
 class Purpose(models.Model):
     title = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="purposes")
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
