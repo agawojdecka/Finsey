b795308aaf9f387165960bce475ddea680a98ec0
agnieszkawojdecka <agnieszka.wojdecka@wp.pl>
2024-12-12 10:36:39 +0100

    Update transactions admin panel with Category model

--- apps/transactions/admin.py
+++ apps/transactions/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from apps.transactions.models import Transaction
+from apps.transactions.models import Transaction, Category
 
 admin.site.register(Transaction)
+admin.site.register(Category)
