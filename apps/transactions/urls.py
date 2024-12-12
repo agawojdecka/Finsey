6d5fee73f185716787b397f8dfb9672cd40a6f47
agnieszkawojdecka <agnieszka.wojdecka@wp.pl>
2024-12-12 12:47:07 +0100

    Refactor

--- apps/transactions/urls.py
+++ apps/transactions/urls.py
@@ -11,9 +11,5 @@ urlpatterns = [
     path("", TransactionListCreateView.as_view(), name="transaction-list-create"),
     path("<int:pk>/", TransactionDetailView.as_view(), name="transaction-detail"),
     path("categories/", CategoryListCreateView.as_view(), name="category-list-create"),
-    path(
-        "categories/<int:pk>/",
-        CategoryDetailView.as_view(),
-        name="category-list-create",
-    ),
+    path("categories/<int:pk>/", CategoryDetailView.as_view(), name="category-list-create"),
 ]
