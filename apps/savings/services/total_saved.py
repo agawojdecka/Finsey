c25d117929e373f0ccba6d02e6a1e6ded552f919
agnieszkawojdecka <agnieszka.wojdecka@wp.pl>
2025-01-12 22:05:30 +0100

    Fix circular import problem

--- apps/savings/services/total_saved.py
+++ apps/savings/services/total_saved.py
@@ -1,7 +1,5 @@
-from apps.savings.models import Saving
-
-
 def calculate_total_saved(goal):
+    from apps.savings.models import Saving
     total_saved = sum(
         saving.amount for saving in goal.savings.filter(operation_type=Saving.Types.INFLOW)
     ) - sum(
