58ce00cd120911ab157cf3a0bcbd27447850aa55
agnieszkawojdecka <agnieszka.wojdecka@wp.pl>
2025-01-23 16:02:46 +0100

    Add inactivity_notification_sent_at attribute to the User model

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class User(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
+    inactivity_notification_sent_at = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.username}"
