8d0bc3c9fcf7a1579c7bf918475c57e2b8f2d342
agnieszkawojdecka <agnieszka.wojdecka@wp.pl>
2025-01-23 15:58:09 +0100

    Create send monthly balance notification celery task

---
+++ apps/transactions/services/notifications.py
@@ -0,0 +1,48 @@
+from datetime import timedelta
+
+from django.core.mail import EmailMessage
+from django.db.models import Sum
+from django.utils import timezone
+
+from apps.core.utils.date import get_first_day_of_month
+from apps.transactions.models import Transaction
+from apps.users.models import User
+
+
+def send_monthly_balance_notification():
+    current_date = timezone.now().date()
+
+    last_day_of_settlement_period = get_first_day_of_month(current_date) - timedelta(days=1)
+    first_day_of_settlement_period = last_day_of_settlement_period.replace(day=1)
+
+    users = User.objects.prefetch_related('transactions').all()
+
+    for user in users:
+        transactions = user.transactions.filter(user=user, date__gte=first_day_of_settlement_period,
+                                                date__lte=last_day_of_settlement_period)
+        monthly_incomes = \
+            transactions.filter(transaction_type=Transaction.Types.INCOME).aggregate(total=Sum('amount'))[
+                'total'] or 0
+        monthly_expenses = \
+            transactions.filter(transaction_type=Transaction.Types.EXPENSE).aggregate(total=Sum('amount'))[
+                'total'] or 0
+        monthly_balance = monthly_incomes - monthly_expenses
+
+        if monthly_balance > 0:
+            subject = f"Congratulations, you have reached a positive monthly balance!"
+        elif monthly_balance == 0:
+            subject = f"After all the expenses, we managed to break even!"
+        else:
+            subject = f"Oops! Your monthly balance is below zero..."
+
+        body = f"Your monthly balance for period {first_day_of_settlement_period} - {last_day_of_settlement_period} is {monthly_balance}."
+        from_email = "your-email@gmail.com"
+        to_email = [user.email]
+
+        email = EmailMessage(subject, body, from_email, to_email)
+
+        try:
+            email.send(fail_silently=False)
+            print(f"Notification sent successfully to user: {user.id}.")
+        except Exception as e:
+            print(f"Failed to send email: {e}")
--- apps/transactions/tasks.py
+++ apps/transactions/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 
 from apps.transactions.services.report import generate_and_send_report, generate_monthly_expense_report
+from apps.transactions.services.notifications import send_monthly_balance_notification
 
 
 @shared_task
@@ -15,3 +16,8 @@ def generate_monthly_expense_report_task(user_id):
     print(f"Generating monthly expense report for user_id: {user_id}")
     generate_monthly_expense_report(user_id)
     print(f"Monthly expense report for user {user_id} created successfully.")
+
+
+@shared_task
+def send_monthly_balance_notification_task():
+    send_monthly_balance_notification()
