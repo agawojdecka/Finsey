94181175150300e32bee5c4ab764f2d1dddc49ed
agnieszkawojdecka <agnieszka.wojdecka@wp.pl>
2025-01-23 23:06:12 +0100

    Refactor send_inactivity_notification_task

--- apps/users/services/inactivity_notifications.py
+++ apps/users/services/inactivity_notifications.py
@@ -1,9 +1,9 @@
 from django.core.mail import EmailMessage
-
+from apps.users.models import User
 
 def send_inactivity_notification(user):
     subject = f"Inactivity Notification - user ID: {user.id}"
-    body = "You haven't logged into your account for 30 days."
+    body = "You haven't logged into your account for 30 days. If you do not log in for the next 30 days, your account will be deleted."
     from_email = "your-email@gmail.com"
     to_email = [user.email]
 
@@ -14,3 +14,29 @@ def send_inactivity_notification(user):
         print("Notification sent successfully.")
     except Exception as e:
         print(f"Failed to send email: {e}")
+
+
+def send_account_deletion_notification(user):
+    subject = f"Account Deletion Notification - user ID: {user.id}"
+    body = "You haven't logged into your account for 30 days since getting inactivity notification. Your account is being deleted."
+    from_email = "your-email@gmail.com"
+    to_email = [user.email]
+
+    email = EmailMessage(subject, body, from_email, to_email)
+
+    try:
+        email.send(fail_silently=False)
+        print("Notification sent successfully.")
+    except Exception as e:
+        print(f"Failed to send email: {e}")
+
+
+def update_sent_inactivity_notification_status_task():
+    users = list(User.objects.filter(sent_inactivity_notification__isnull=False))
+    users_to_update = []
+    for user in users:
+        if user.inactivity_notification_sent_at is not None and user.last_login >= user.inactivity_notification_sent_at:
+            user.inactivity_notification_sent_at = None
+            users_to_update.append(user)
+
+    User.objects.bulk_update(users_to_update, ['inactivity_notification_sent_at'])
--- apps/users/tasks.py
+++ apps/users/tasks.py
@@ -4,24 +4,31 @@ from celery import shared_task
 from django.utils import timezone
 
 from apps.users.models import User
-from apps.users.services.inactivity_notifications import send_inactivity_notification
-
-
-@shared_task
-def test_celery_beat():
-    """
-    Simple task to test Celery beat
-    """
-    print("Task completed")
+from apps.users.services.inactivity_notifications import send_inactivity_notification, \
+    send_account_deletion_notification, update_sent_inactivity_notification_status_task
 
 
 @shared_task
 def send_inactivity_notification_task():
     """
-    Sends email to all users that haven't been active since 30 days
+    1. Updates inactivity notification status if user logged in after sending inactivity notification.
+    2. Sends email to all users that haven't been active for 30 days about inactivity and potential account deletion.
+    3. After next 30 days with no activity, sends deletion notification and deletes user.
     """
-    inactive_users = list(User.objects.filter(last_login__lte=timezone.now() - timedelta(days=30)))
+    update_sent_inactivity_notification_status_task()
+
+    now = timezone.now().date()
+    thirty_days_ago = timedelta(days=30)
+
+    inactive_users = list(User.objects.filter(last_login__lte=now - thirty_days_ago))
 
     for user in inactive_users:
-        send_inactivity_notification(user)
-        print(f"Inactivity notification sent to user {user.id}")
+        if user.inactivity_notification_sent_at is None:
+            send_inactivity_notification(user)
+            print(f"Inactivity notification sent to user {user.id}")
+            user.inactivity_notification_sent_at = now
+            user.save()
+        elif now - user.inactivity_notification_sent_at >= timedelta(days=30):
+            send_account_deletion_notification(user)
+            print(f"Account deletion notification sent to user {user.id}")
+            user.delete()
