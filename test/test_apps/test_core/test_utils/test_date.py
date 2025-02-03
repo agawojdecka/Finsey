2ac801deb34d6667f5c2464896ad0f3cece7c86f
agnieszkawojdecka <agnieszka.wojdecka@wp.pl>
2025-02-03 11:29:27 +0100

    Parametrize date test

--- test/test_apps/test_core/test_utils/test_date.py
+++ test/test_apps/test_core/test_utils/test_date.py
@@ -1,11 +1,21 @@
-from apps.core.utils.date import *
+from datetime import date
+
+import pytest
+
+from apps.core.utils.date import get_first_day_of_month, get_last_day_of_month
+
 
 def test_get_first_day_of_month():
-    day = date(2024, 2,15)
+    day = date(2024, 2, 15)
     assert get_first_day_of_month(day) == date(2024, 2, 1)
 
-def test_get_last_day_of_month():
-    day_1 = date(2024, 2,15)
-    day_2 = date(2024, 12,1)
-    assert get_last_day_of_month(day_1) == date(2024, 2, 29)
-    assert get_last_day_of_month(day_2) == date(2024, 12, 31)
+
+@pytest.mark.parametrize(
+    "day,expected_day",
+    [
+        (date(2024, 2, 15), date(2024, 2, 29)),
+        (date(2024, 12, 1), date(2024, 12, 31)),
+    ]
+)
+def test_get_last_day_of_month(day, expected_day):
+    assert get_last_day_of_month(day) == expected_day
