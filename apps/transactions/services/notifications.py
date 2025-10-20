bb62d32806052525c0e761a75fbbecdabd4cf501
agnieszkawojdecka <agnieszka.wojdecka@wp.pl>
2025-10-20 11:10:08 +0200

    fix: prefetch_related usage in user transactions

--- apps/transactions/services/notifications.py
+++ apps/transactions/services/notifications.py
@@ -1,12 +1,15 @@
 from datetime import timedelta
+from logging import getLogger
 
 from django.core.mail import EmailMessage
-from django.db.models import Sum
+from django.db.models import Prefetch, Sum
 from django.utils import timezone
 
 from apps.core.utils.date import get_first_day_of_month
 from apps.transactions.models import Transaction
 from apps.users.models import User
+
+logger = getLogger(__name__)
 
 
 def send_monthly_balance_notification() -> None:
@@ -20,19 +23,24 @@ def send_monthly_balance_notification() -> None:
     last_day_of_settlement_period = get_first_day_of_month(current_date) - timedelta(days=1)
     first_day_of_settlement_period = last_day_of_settlement_period.replace(day=1)
 
-    users = User.objects.prefetch_related('transactions').all()
+    users = User.objects.prefetch_related(
+        Prefetch(
+            "transactions",
+            queryset=Transaction.objects.filter(
+                date__gte=first_day_of_settlement_period,
+                date__lte=last_day_of_settlement_period,
+            ),
+        )
+    )
 
     for user in users:
-        transactions = user.transactions.filter(
-            user=user,
-            date__gte=first_day_of_settlement_period,
-            date__lte=last_day_of_settlement_period,
-        )
         monthly_incomes = (
-            transactions.filter(transaction_type=Transaction.Types.INCOME).aggregate(total=Sum('amount'))['total'] or 0
+            user.transactions.filter(transaction_type=Transaction.Types.INCOME).aggregate(total=Sum('amount'))['total']
+            or 0
         )
         monthly_expenses = (
-            transactions.filter(transaction_type=Transaction.Types.EXPENSE).aggregate(total=Sum('amount'))['total'] or 0
+            user.transactions.filter(transaction_type=Transaction.Types.EXPENSE).aggregate(total=Sum('amount'))['total']
+            or 0
         )
         monthly_balance = monthly_incomes - monthly_expenses
 
@@ -54,6 +62,6 @@ def send_monthly_balance_notification() -> None:
 
         try:
             email.send(fail_silently=False)
-            print(f"Notification sent successfully to user: {user.id}.")
+            logger.info(f"Notification sent successfully to user: {user.id}.")
         except Exception as e:
-            print(f"Failed to send email: {e}")
+            logger.error(f"Failed to send email: {e}")
